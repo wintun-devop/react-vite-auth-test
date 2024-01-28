108a123e81e23b9251aa91d50e5bc5a5d1a8a36f
Win Tun <wintun.hlaing@outlook.com>
2024-01-28 19:44:18 +0630

    test build

--- react-front-end/src/main.tsx
+++ react-front-end/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom';
 
 
 // ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -15,6 +16,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
         <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
